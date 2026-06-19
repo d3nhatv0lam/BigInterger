0e1d69f4076deba794dd222933975345775e7a14
Duong Minh Duc <110104167+d3nhatv0lam@users.noreply.github.com>
2026-06-19 13:32:09 +0700

    feat: add unit test project and update `Bignum` constructor for string input validation

--- Bignum/Core/Bignum/Bignum.cs
+++ Bignum/Core/Bignum/Bignum.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Bignum.Core.Bignum;
@@ -50,9 +51,77 @@ public class Bignum
     public Bignum(string value)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(value);
-        ArgumentOutOfRangeException.ThrowIfGreaterThan(value.Length, BignumConstants.MaxDigitOfBignum);
+        value = value.Trim();
 
-        throw new NotImplementedException();
+        var isNegative = false;
+        var startIndex = 0;
+
+        switch (value[0])
+        {
+            case '-':
+                isNegative = true;
+                startIndex = 1;
+                break;
+            case '+':
+                startIndex = 1;
+                break;
+        }
+        
+        // chỉ có dấu
+        if (startIndex >= value.Length)
+        {
+            throw new FormatException("Input string was not in a correct format.");
+        }
+        
+        var digitCount = value.Length - startIndex;
+        if (digitCount > BignumConstants.MaxDigitOfBignum)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), $"Number of digits exceeds maximum allowed ({BignumConstants.MaxDigitOfBignum}).");
+        }
+
+        for (var i = startIndex; i < value.Length; i++)
+        {
+            if (!char.IsDigit(value[i])) throw new FormatException("Input string was not in a correct format.");
+        }
+        
+        // cắt số 0 ở đầu
+        while (startIndex < value.Length && value[startIndex] == '0')
+        {
+            startIndex++;
+        }
+
+        // node 0
+        if (startIndex >= value.Length)
+        {
+            Head = null;
+            Tail = null;
+            NodeCount = 0;
+            return;
+        }
+        
+        this.IsNegative = isNegative;
+        
+        var dummy = new BignumNode(0);
+        var current = dummy;
+        var count = 0;
+        
+        var end = value.Length;
+        while (startIndex < end)
+        {
+            var start = Math.Max(startIndex, end - BignumConstants.NodeDigitCount);
+            var blockStr = value.Substring(start, end - start);
+            var nodeParse = int.Parse(blockStr);
+            
+            current.Next = new BignumNode(nodeParse);
+            current = current.Next;
+            count++;
+            
+            end -= BignumConstants.NodeDigitCount;
+        }
+        
+        Head = dummy.Next;
+        NodeCount = count;
+        InitTail();
     }
 
     private Bignum(bool isNegative, BignumNode? head, int nodeCount)
@@ -169,24 +238,22 @@ public class Bignum
     {
         if (Head is null) return "0";
 
-        var builder = new StringBuilder();
+        var parts = new List<string>(NodeCount);
         var current = Head;
 
         while (current is not null)
         {
-            if (current.Next is null)
-            {
-                builder.Append(current.Value);
-            }
-            else
-            {
-                builder.Append(current.Value.ToString(BignumConstants.BignumFormat));
-            }
+            parts.Add(current.Next is null
+                ? current.Value.ToString()
+                : current.Value.ToString(BignumConstants.BignumFormat));
 
             current = current.Next;
         }
 
-        return InPlaceReverse(builder);
+        parts.Reverse();
+        
+        var sign = IsNegative ? "-" : "";
+        return sign + string.Concat(parts);
     }
 
     private int GetNodeCount()
@@ -213,21 +280,5 @@ public class Bignum
         }
 
         this.Tail = current;
-    }
-
-    private static string InPlaceReverse(StringBuilder sb)
-    {
-        int startIndex = 0;
-        int endIndex = sb.Length - 1;
-
-        while (startIndex < endIndex)
-        {
-            (sb[startIndex], sb[endIndex]) = (sb[endIndex], sb[startIndex]);
-
-            startIndex++;
-            endIndex--;
-        }
-
-        return sb.ToString();
     }
 }
